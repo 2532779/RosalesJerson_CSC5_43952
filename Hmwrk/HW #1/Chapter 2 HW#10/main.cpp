0eddc624ca9159c75f6ca48e14d88deec983a521
2532779 <rjerson20@gmail.com>
2015-03-04 10:09:44 -0800

    Signed-off-by: 2532779 <rjerson20@gmail.com>

---
+++ Hmwrk/HW #1/Chapter 2 HW#10/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Jerson Rosales
+ * Created on March 4, 2015, 9:55 AM
+ * Purpose: Homework problem for number 10!
+ */
+
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Libraries
+
+//Function Prototypes
+
+//Execution begins
+
+int main(int argc, char** argv) {
+    float mil,gal,mpg;//Miles,Gallons,MPG
+    mil=375;//How many miles on a full tank
+    gal=15;//How many gallons the tank can hold
+    mpg=mil/gal;
+    cout<<"The car can drive "<<mpg<<" MPG!"<<endl;
+    return 0;
+}
+
