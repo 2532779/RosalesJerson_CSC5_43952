2ac55303ddc0e40a888747500aec2ac61f454a55
2532779 <rjerson20@gmail.com>
2015-03-04 09:53:42 -0800

    Signed-off-by: 2532779 <rjerson20@gmail.com>

--- Hmwrk/HW #1/Chapter 2 HW#1/main.cpp
+++ Hmwrk/HW #1/Chapter 2 HW#1/main.cpp
@@ -1,18 +1,18 @@
 /* 
- * File: Program for problem number 1!
+ * File: main.cpp
  * Author: Jerson Rosales
- *
  * Created on February 22, 2015, 1:28 PM
+ * Purpose: Problem number 1 
  */
 
 #include <iostream>
 
 using namespace std;
 int main(int argc, char** argv) {
-    int apples, bannanas, total;
-    apples = 50;
-    bannanas = 100;
-    total = apples + bannanas;
+    unsigned short apls, bnans, total;// Variables are Apples and bannanas 
+    apls = 50;
+    bnans = 100;
+    total = apls + bnans;
     cout << "The sum of 50 apples and 100 bannanas is " << total << endl;
     cout << "apples and bannanas." << endl;
          
---
+++ Hmwrk/HW #1/Chapter 2 HW#9/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Jerson Rosales
+ * Created on March 4, 2015, 9:39 AM
+ * Purpose: Homework number 9
+ */
+
+#include <iostream>
+
+using namespace std;
+
+//User libraries
+
+//Global libraries
+
+//Function Prototypes
+
+//Execution Begins
+
+
+int main(int argc, char** argv) {
+    // I Dont have to declare variables!Because im only naming sizeof 
+    //data types!
+    cout<<"The amount the data type 'char' has is "<< sizeof(char)<<
+            " byte."<<endl;
+    cout<<"The amount the data type 'int'has is "<< sizeof(int)<<
+            " bytes."<<endl;
+    cout<<"The amount the data type 'float' has is "<< sizeof(float)<<
+            " bytes."<<endl;
+    cout<<"The amount of data type 'double'has is "<< sizeof(double)<<
+            " bytes."<<endl;
+
+    return 0;
+}
+
