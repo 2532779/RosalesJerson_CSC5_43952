06a3e78ae590cbc23d0a4882946cf2296badc1b3
2532779 <rjerson20@gmail.com>
2015-03-05 18:41:48 -0800

    Signed-off-by: 2532779 <rjerson20@gmail.com>

--- Hmwrk/HW #1/Chapter 2 HW#8/main.cpp
+++ Hmwrk/HW #1/Chapter 2 HW#8/main.cpp
@@ -7,22 +7,29 @@
 #include <iostream>
 
 using namespace std;
+//User Libraries
+
+//Global Functions
+unsigned char CNVPCT=100;
+//Function Prototypes
+
+//Execution Begins
 
 
 int main(int argc, char** argv) {
     
     
-    float priceOfItem1 = 15.95,
-            priceOfItem2 = 24.95,
-            priceOfItem3 = 6.95,
-            priceOfItem4 = 12.95,
-            priceOfItem5 = 3.95,
-            tax = .07,
-            total1,//The total with tax (repeats to total5)
-            total2,
-            total3,
-            total4,
-            total5,
+    float prcOI1 = 15.95,//Price of item 1
+            prcOI2 = 24.95,//Price of item 2
+            prcOI3 = 6.95,//Price of item 3
+            prcOI4 = 12.95,//Price of item 4
+            prcOI5 = 3.95,//Price of item 5
+            tax = 7,//Tax
+            ttl1,//The total with tax (repeats to ttl5)
+            ttl2,
+            ttl3,
+            ttl4,
+            ttl5,
             tax1,// The tax with the purchases (repeats to tax5)
             tax2,
             tax3,
@@ -30,43 +37,43 @@ int main(int argc, char** argv) {
             tax5;
     
     
-    tax1 = priceOfItem1 * tax;// Price of taxed item (repeats to tax5)
-    tax2 = priceOfItem2 * tax;
-    tax3 = priceOfItem3 * tax;
-    tax4 = priceOfItem4 * tax;
-    tax5 = priceOfItem5 * tax;
+    tax1 = prcOI1 * tax/CNVPCT;// Price of taxed item (repeats to tax5)
+    tax2 = prcOI2 * tax/CNVPCT;
+    tax3 = prcOI3 * tax/CNVPCT;
+    tax4 = prcOI4 * tax/CNVPCT;
+    tax5 = prcOI5 * tax/CNVPCT;
     
-    total1 = priceOfItem1 + tax1;//Total With Purchase and Tax!
-    total2 = priceOfItem2 + tax2;
-    total3 = priceOfItem3 + tax3;
-    total4 = priceOfItem4 + tax4;
-    total5 = priceOfItem5 + tax5;
+    ttl1 = prcOI1 + tax1;//Total With Purchase and Tax!
+    ttl2 = prcOI2 + tax2;
+    ttl3 = prcOI3 + tax3;
+    ttl4 = prcOI4 + tax4;
+    ttl5 = prcOI5 + tax5;
     
     
     cout << "Item 1: \n";
-    cout << "Subtotal: " << priceOfItem1 << endl;
+    cout << "Subtotal: " << prcOI1 << endl;
     cout << "Sales Tax: " << tax1 << endl;
-    cout << "Total: " << total1 << endl;
-    
+    cout << "Total: " << ttl1 << endl;
+    cout << endl;
     cout << "Item 2: \n";
-    cout << "Subtotal: " << priceOfItem2 << endl;
+    cout << "Subtotal: " << prcOI2 << endl;
     cout << "Sales Tax: " << tax2 << endl;
-    cout << "Total: " << total2 << endl;
-    
+    cout << "Total: " << ttl2 << endl;
+    cout << endl;
     cout << "Item 3: \n";
-    cout << "Subtotal: " << priceOfItem3 << endl;
+    cout << "Subtotal: " << prcOI3 << endl;
     cout << "Sales Tax: " << tax3 << endl;
-    cout << "Total: " << total3 << endl;
-    
+    cout << "Total: " << ttl3 << endl;
+    cout << endl;
     cout << "Item 4: \n";
-    cout << "Subtotal: " << priceOfItem4 << endl;
+    cout << "Subtotal: " << prcOI4 << endl;
     cout << "Sales Tax: " << tax4 << endl;
-    cout << "Total: " << total4 << endl;
-    
+    cout << "Total: " << ttl4 << endl;
+    cout << endl;
     cout << "Item 5: \n";
-    cout << "Subtotal: " << priceOfItem5 << endl;
+    cout << "Subtotal: " << prcOI5 << endl;
     cout << "Sales Tax: " << tax5 << endl;
-    cout << "Total: " << total5 << endl;
+    cout << "Total: " << ttl5 << endl;
             return 0;
 }
 
