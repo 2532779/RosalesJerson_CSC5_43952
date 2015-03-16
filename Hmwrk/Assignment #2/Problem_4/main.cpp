c9075abb97750961b51ab9d4d64d5da54c5879ff
2532779 <rjerson20@gmail.com>
2015-03-15 20:42:38 -0700

    Signed-off-by: 2532779 <rjerson20@gmail.com>

---
+++ Hmwrk/Assignment #2/Problem_4/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Jerson Rosales
+ *Created on March 15, 2015, 8:34 PM
+ * Purpose:Average Rainfall in 3 Months
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins
+int main(int argc, char** argv) {
+    unsigned char nm1,nm2,nm3,m1,m2,m3;
+    float avg;
+    cout<<"Type name of Month 1:"<<endl;
+    cin>>nm1;
+    cout<<""
+
+    return 0;
+}
+
