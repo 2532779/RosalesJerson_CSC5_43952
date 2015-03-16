d1e74178264c0bbf126593922a9581fa9244d981
2532779 <rjerson20@gmail.com>
2015-03-15 20:03:32 -0700

    Signed-off-by: 2532779 <rjerson20@gmail.com>

---
+++ Hmwrk/Assignment #2/Problem_2/main.cpp
@@ -0,0 +1,49 @@
+/* 
+ * File:   main.cpp
+ * Author: Jerson Rosales
+ * Created on March 15, 2015, 7:31 PM
+ * Purpose:Calculation Softball ticket sales 
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins!
+int main(int argc, char** argv) {
+    //Seat A Cost is 15,Seat B Cost is 12,Seat C cost is 9
+    unsigned char stAC=15,stBC=12,stCC=9;
+    //Variables for inputs
+    unsigned short stA,stB,stC;
+    //Variables for total 
+    float t1,t2,t3,fT;
+    cout<<"Number of seats sold for Class A:"<<endl;
+    cin>>stA;
+    t1=stA*stAC;
+    cout<<"Number of seats sold for Class B:"<<endl;
+    cin>>stB;
+    t2=stB*stBC;
+    cout<<"Number of seats sold for Class C:"<<endl;
+    cin>>stC;
+    t3=stC*stCC;
+    fT=t1+t2+t3;
+    cout<<setprecision(2)<<fixed;
+    cout<<"Class A Seats:"<<stA<<endl;
+    cout<<"Class B Seats:"<<stB<<endl;
+    cout<<"Class C Seats:"<<stC<<endl;
+    cout<<"Total Sales:"<<fT<<endl;
+    
+    
+    
+    
+
+    return 0;
+}
+
