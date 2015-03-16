38788df72c1c690ff7018177695f1ebbe1c6dd58
2532779 <rjerson20@gmail.com>
2015-03-16 09:57:01 -0700

    Signed-off-by: 2532779 <rjerson20@gmail.com>

--- Hmwrk/Assignment #2/Problem_4/main.cpp
+++ Hmwrk/Assignment #2/Problem_4/main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
@@ -18,11 +19,31 @@ using namespace std;
 
 //Execution Begins
 int main(int argc, char** argv) {
-    unsigned char nm1,nm2,nm3,m1,m2,m3;
-    float avg;
+    //Name of month 1,2,3;rainfall of month 1,2,3
+    //Average as well
+    string nm1,nm2,nm3;
+    float m1,m2,m3,avg;
+    //Input the information
     cout<<"Type name of Month 1:"<<endl;
     cin>>nm1;
-    cout<<""
+    cout<<"Type rainfall in inches xx.x"<<endl;
+    cin>>m1;
+    cout<<"Type name of Month 2:"<<endl;
+    cin>>nm2;
+    cout<<"Type rainfall in inches xx.x"<<endl;
+    cin>>m2;
+    cout<<"Type name of month 3:"<<endl;
+    cin>>nm3;
+    cout<<"Type rainfall in inches xx.x"<<endl;
+    cin>>m3;
+    avg=(m1+m2+m3)/3;
+    //Output The results
+    cout<<"In the month of "<<nm1<<" there was "<<m1<<" inches of rain"<<endl;
+    cout<<"In the month of "<<nm2<<" there was "<<m2<<" inches of rain"<<endl;
+    cout<<"In the month of "<<nm3<<" there was "<<m3<<" inches of rain"<<endl;
+    cout<<setprecision(1)<<fixed;
+    cout<<"Average rainfall: "<<avg<<" inches"<<endl;
+    
 
     return 0;
 }
