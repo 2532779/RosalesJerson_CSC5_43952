ac2136fdd5f8b28d437544a6195a6fff58f93028
2532779 <rjerson20@gmail.com>
2015-03-17 18:07:52 -0700

    Signed-off-by: 2532779 <rjerson20@gmail.com>

--- Hmwrk/Assignment #2/Problem_7/main.cpp
+++ Hmwrk/Assignment #2/Problem_7/main.cpp
@@ -7,20 +7,53 @@
 
 //System Libraries
 #include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
 
 //User Libraries
 
 //Global Constants
+unsigned char CNVPCT=100;
 
 //Function Prototypes
 
 //Execution begins!
 
-using namespace std;
 
 
 int main(int argc, char** argv) {
-
-    return 0;
+    //Declare Variables 
+    //Movie Name
+    string nM;
+    //Variables for Adult tickets and Children tickets and Costs plus totals
+    unsigned short nA,nC,cA=10,cC=6,taT,tcT;
+    //Variables for Gross,Net, amount paid to distributor,total gross
+    float gboP,nboP,amtDist;
+    //Variables for net 
+    //Prompt User for inputs
+    cout<<"What is the name of the movie?"<<endl;
+    getline(cin,nM);
+    cout<<"How many adult tickets were sold?"<<endl;
+    cin>>nA;
+    cout<<"How many children tickets sold?"<<endl;
+    cin>>nC;
+    //Calculations
+    taT=nA*cA;
+    tcT=nC*cC;
+    gboP=taT+tcT;
+    nboP=(gboP)*(20/static_cast<float>(CNVPCT));
+    amtDist=gboP-nboP;
+    //Output the results
+    cout<<"Name of Movie: "<<setw(17)<<nM<<endl;
+    cout<<"Adult tickets sold: "<<setw(12)<<nA<<endl;
+    cout<<"Child ticket sold: "<<setw(13)<<nC<<endl;
+    cout<<setprecision(2)<<fixed<<showpoint;
+    cout<<"Gross Box office Profits: "<<setw(3)<<"$"<<gboP<<endl;
+    cout<<"Net Box Office Profit: "<<setw(6)<<"$"<<nboP<<endl;
+    cout<<"Amount Paid to Distributor: "<<"$"<<amtDist<<endl;
+   
+  return 0;
 }
 
