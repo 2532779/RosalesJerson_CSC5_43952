716555d2e4d49f6b6d42e980e0d53dd81a87933b
2532779 <rjerson20@gmail.com>
2015-04-06 11:40:21 -0700

    Signed-off-by: 2532779 <rjerson20@gmail.com>

---
+++ Hmwrk/Assignment 4/Gaddis_8thEd_Chap5_Prob6/main.cpp
@@ -0,0 +1,52 @@
+/* 
+ * File:   main.cpp
+ * Author: Jerson Rosales
+ * Created on April 6, 2015, 9:36 AM
+ * Purpose:Determine how many miles driven in an hour
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins here
+
+
+int main(int argc, char** argv) {
+    //Declare Variables
+    float spd,hrsDvn,dstnce,hrs=1;
+    //Prompt user for inputs
+    cout<<"How many hours have you driven?"<<endl;
+    cin>>hrsDvn;
+    while (hrsDvn<0)
+    {
+        cout<<"ERROR!.....Input a number greater than zero"<<endl;
+        cin>>hrsDvn;
+    }
+    cout<<"What was your average speed each hour?"<<endl;
+    cin>>spd;
+    while (spd<0)
+    {
+        cout<<"ERROR!.....Input a number greater than zero"<<endl;
+        cin>>spd;
+    }
+    cout<<"Hour  "<<"   "<<"Miles Driven  "<<endl;
+    cout<<"-------------------------------"<<endl;
+    while (hrs<=hrsDvn)
+    {
+        dstnce+=spd;
+        cout<<hrs<<"        "<<dstnce<<endl;
+        hrs++;
+                
+    }
+
+    return 0;
+}
+
