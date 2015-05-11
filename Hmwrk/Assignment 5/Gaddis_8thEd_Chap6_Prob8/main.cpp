b3d768f142967bbea3b5f8a382ecb8d67c61f677
2532779 <rjerson20@gmail.com>
2015-05-11 10:34:12 -0700

    Signed-off-by: 2532779 <rjerson20@gmail.com>

--- Hmwrk/Assignment 5/Gaddis_8thEd_Chap6_Prob8/main.cpp
+++ Hmwrk/Assignment 5/Gaddis_8thEd_Chap6_Prob8/main.cpp
@@ -1,17 +1,21 @@
 /* 
  * File:   main.cpp
  * Author: Jerson Rosales
- *
  * Created on May 11, 2015, 10:26 AM
  */
 
-#include <cstdlib>
+//System Libraries
+#include <iostream>
 
 using namespace std;
 
-/*
- * 
- */
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins here
 int main(int argc, char** argv) {
 
     return 0;
