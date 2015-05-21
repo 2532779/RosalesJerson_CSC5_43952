cebdb7bd82bf805b73446a224651e3e09a1c49e5
2532779 <rjerson20@gmail.com>
2015-05-21 11:37:56 -0700

    Signed-off-by: 2532779 <rjerson20@gmail.com>

---
+++ Hmwrk/Assignment 6/Gaddis_8thEd_Chap7_Prob3/main.cpp
@@ -0,0 +1,43 @@
+/* 
+ * File:   main.cpp
+ * Author: Jerson Rosales
+ * Created on May 21, 2015, 11:16 AM
+ */
+
+//System Libraries
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+//User Libraries
+
+//GLobal Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare Variables
+    int count;
+    int SIZE=5;
+    string salsa[]={"Mild","Medium","Sweet","Hot","Zesty"};
+    int jars[SIZE];
+    //Prompt the user for inputs
+    cout<<"Type in how many jars sold for each of the spices"<<endl;
+    for(count=0;count<=4;count++){
+        cout<<salsa[count]<<" "<<endl;
+        cin>>jars[count];
+    }
+    cout<<"Salsa"<<"   "<<"Jars Sold"<<endl;
+    cout<<"-------------------------"<<endl;
+ 
+    //Outputting the results
+    for(count=0;count<=4;count++){
+        cout<<salsa[count]<<"        "<<jars[count]<<endl;
+        
+    }
+    return 0;
+}
+
