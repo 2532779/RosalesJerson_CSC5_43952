8ee45a2a764b0f58451b0e3b94ddcd7a80a89453
2532779 <rjerson20@gmail.com>
2015-05-21 11:02:40 -0700

    Signed-off-by: 2532779 <rjerson20@gmail.com>

--- Hmwrk/Assignment 6/Gaddis_8thEd_Chap7_Prob2/main.cpp
+++ Hmwrk/Assignment 6/Gaddis_8thEd_Chap7_Prob2/main.cpp
@@ -20,17 +20,38 @@ int main(int argc, char** argv) {
     //Declare Variables
     int SIZE=12;
     float rain[SIZE];
-    int day,year;
+    float total=0,avg;
+    float highest,lowest;
     int count;
     string month[SIZE]={"Janurary","Feburary","March","April","May","June","July",
                       "August","September","October","November","December"};
-    float highest,lowest;
     //Prompt user for the rain each month
     cout<<"Input how many inches of rain fell each month..."<<endl;
     for(count=0;count<=11;count++){
         cout<<month[count]<<endl;
         cin>>rain[count];
     }
+    //Gathering the total rainfall all year
+    for (count=1;count<=11;count++){
+        total+=rain[count];
+    }
+    avg=total/SIZE;
+    cout<<"The Average rain fall yearly is "<<avg<<" inches"<<endl;
+    //Getting the lowest and highest values
+    highest=rain[0];
+    for(count=0;count<=11;count++){
+        if(rain[count]>highest){
+            highest=rain[count];
+        }
+    }
+    cout<<"The highest rainfall all year is  "<<highest<<endl;
+    lowest=rain[0];
+    for(count=0;count<=11;count++){
+        if(rain[count]<lowest){
+            lowest=rain[count];
+        }
+    }
+    cout<<"The lowest rainfall all year is "<<lowest<<endl;
     
     return 0;
 }
